edc25441c075bc65a06f2f51d1454d8f69f9e466
JandersonCRB <janderson@viajamont.com>
2018-02-26 18:01:55 -0300

    Installed nuka-carousel

--- src/components/Experiences/Show/Show.jsx
+++ src/components/Experiences/Show/Show.jsx
@@ -1,44 +1,60 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
+import Carousel from 'nuka-carousel';
 
 @inject('experience') @observer
 export default class Show extends React.Component {
 
-	componentWillMount() {
-		const { experience } = this.props;
-		experience.findBy({ id: this.props.params.experienceId }, {
-			200: (body) => {
-				experience.setSelected(body);
-				this.setState({ values: body });
-			},
-			404: () => { this.notFound = true; }
-		}); //GET REQUEST
-	}
+  componentWillMount() {
+    const { experience } = this.props;
+    experience.findBy({ id: this.props.params.experienceId }, {
+      200: (body) => {
+        experience.setSelected(body);
+        this.setState({ values: body });
+      },
+      404: () => { this.notFound = true; }
+    }); //GET REQUEST
+  }
 
-	renderTopics(selected) {
-		let buffer = [];
+  renderTopics(selected) {
+    let buffer = [];
 
-		if (selected.description) {
-			buffer.push(
-			<div className="experience-topic" style={{whiteSpace: 'pre-line'}}>
-				<h4>A Experiência</h4>
-				{selected.description}
-			</div>
-		);
-		}
-		return buffer;
-	}
+    if (selected.description) {
+      buffer.push(
+        <div className="experience-topic" style={{ whiteSpace: 'pre-line' }}>
+          <h4>A Experiência</h4>
+          {selected.description}
+        </div>
+      );
+    }
+    return buffer;
+  }
 
-	render() {
-		const { selected } = this.props.experience;
-		return (
-			<div id="Show" className="container">
-				<div>
-					<h2><strong>{selected.name}</strong></h2>
-				</div>
-				<hr />
-				{this.renderTopics(selected)}
-			</div>
-		)
-	}
+  renderImages(selected) {
+    return (
+      <Carousel>
+        {selected.photos.map(photo => (
+          <div>
+            <img src={photo.url} />
+            {/* {console.log(photo.url)} */}
+          </div>
+        ))}
+      </Carousel>
+    )
+  }
+  render() {
+    const { selected, isLoading } = this.props.experience;
+    console.log(selected);
+    if (isLoading) return <div>Loading</div>
+    return (
+      <div id="Show" className="container">
+        {this.renderImages(selected)}
+        <div>
+          <h2><strong>{selected.name}</strong></h2>
+        </div>
+        <hr />
+        {this.renderTopics(selected)}
+      </div>
+    )
+  }
 }
