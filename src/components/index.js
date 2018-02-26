a44692ebab952c6a181fca6b5186cfaa56f8fbff
JandersonCRB <janderson@viajamont.com>
2018-02-26 17:27:08 -0300

    changed jsx file extensions

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import Layout from './Layout.jsx';
+import Home from './Home/Home.jsx';
+import Show from './Experiences/Show/Show.jsx';
+import ExperienceNew from './Experiences/New/New.jsx';
+import ExperienceEdit from './Experiences/Edit/Edit.jsx';
+import Experiences from './Experiences/Collection/Collection.jsx';
+
+export default{
+    Layout,
+    Home,
+    Show,
+    ExperienceNew,
+    ExperienceEdit,
+    Experiences
+}
