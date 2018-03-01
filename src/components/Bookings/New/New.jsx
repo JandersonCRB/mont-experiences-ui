eddfb425553dd9b55e4e2122a5101f42d91055c8
JandersonCRB <janderson@viajamont.com>
2018-03-01 18:09:38 -0300

    Implemented Stepper and setup airbnb calendar

--- src/components/Bookings/New/New.jsx
+++ src/components/Bookings/New/New.jsx
@@ -1,20 +1,34 @@
+import 'react-dates/initialize';
 import React, { Component } from 'react'
 
 import { browserHistory } from 'react-router';
 
 import { inject, observer } from 'mobx-react';
 
+import moment from 'moment';
+
+import { SingleDatePicker } from 'react-dates';
+
+import 'react-dates/lib/css/_datepicker.css';
+
+import Stepper, { Step, StepLabel } from 'material-ui/Stepper';
+import Button from 'material-ui/Button';
+
 @inject('experience')
 @inject('booking')
 @inject('session') @observer
 class BookingsNew extends Component {
-	constructor(){
+	
+	constructor() {
 		super();
+		moment.locale('pt-br');
+		console.log(moment());
 		this.state = {
-			values:{
+			values: {
 				name: '',
-
-			}
+				dates: moment()
+			},
+			activeStep: 0
 		}
 	}
 	componentWillMount() {
@@ -22,8 +36,48 @@ class BookingsNew extends Component {
 		experience.findBy({ id: this.props.params.experienceId });
 	}
 
+
+	handleBack = () => {
+		const { activeStep } = this.state;
+		this.setState({
+			activeStep: activeStep - 1,
+		});
+	};
+
+	handleNext = () => {
+		const { activeStep } = this.state;
+		let { skipped } = this.state;
+		this.setState({
+			activeStep: activeStep + 1,
+			skipped,
+		});
+	};
+
+	getStepContent(step) {
+		switch (step) {
+			case 0:
+				return (
+					// https://github.com/airbnb/react-dates
+					<SingleDatePicker
+						date={this.state.values.dates}
+						onDateChange={dates => this.setState({ values: {dates} })}
+						focused={this.state.focused} // PropTypes.bool
+						onFocusChange={({ focused }) => this.setState({ focused })} // PropTypes.func.isRequired
+					/>
+				)
+			case 1:
+				return 'What is an ad group anyways?';
+			case 2:
+				return 'This is the bit I really care about!';
+			default:
+				return 'Unknown step';
+		}
+	};
+
 	render() {
 		const { session } = this.props;
+		const steps = getSteps();
+		const { activeStep } = this.state;
 		if (!session.signedIn && !session.isLoading) {
 			browserHistory.push('/users/sign_in');
 		}
@@ -32,21 +86,49 @@ class BookingsNew extends Component {
 			const experience = this.props.experience;
 			return (
 				<div className='container'>
-					<form>
-						<div className="col-sm-12 col-md-8">
-							<div className="booking--form">
-								<div className="row">
-									<div className="col-md-8">
-										<h3>Informações do Titular do Agendamento</h3>
-										<label>NOME COMPLETO</label><br />
-										<input type='text' value={this.state.values.name} /><br/>
-										<label>EMAIL</label><br />
-										<input type='text' value={this.state.values.email} />
+					<Stepper activeStep={activeStep}>
+						{steps.map((label, index) => {
+							return (
+								<Step key={label}>
+									<StepLabel>{label}</StepLabel>
+								</Step>
+							);
+						})}
+					</Stepper>
+
+					<div>
+						{activeStep === steps.length ? (
+							<div>
+								<span>
+									All steps completed - you&quot;re finished
+              </span>
+								<Button onClick={this.handleReset}>
+									Reset
+              </Button>
+							</div>
+						) : (
+								<div>
+									{this.getStepContent(activeStep)}
+									<div className="row">
+										<Button
+											disabled={activeStep === 0}
+											onClick={this.handleBack}
+										>
+											Voltar
+                </Button>
+										<div className="ml-auto">
+											<Button
+												variant="raised"
+												color="primary"
+												onClick={this.handleNext}
+											>
+												{activeStep === steps.length - 1 ? 'Finalizar' : 'Avançar'}
+											</Button>
+										</div>
 									</div>
 								</div>
-							</div>
-						</div>
-					</form>
+							)}
+					</div>
 				</div>
 			)
 		}
@@ -54,3 +136,7 @@ class BookingsNew extends Component {
 }
 
 export default BookingsNew;
+
+function getSteps() {
+	return ['Escolha a data que melhor se encaixa para você', 'Conte-nos como podemos chegar até você', 'Revise e confirme as informações inseridas'];
+}
