304aa092174c962bb9527038dd199beb0b5896f1
JandersonCRB <janderson@viajamont.com>
2018-03-12 10:37:23 -0300

    Bookings form validations

--- src/components/Bookings/New/New.jsx
+++ src/components/Bookings/New/New.jsx
@@ -16,10 +16,12 @@ import './New.css';
 
 import Location from 'material-ui-icons/LocationOn';
 import Date from 'material-ui-icons/DateRange';
-import People from 'material-ui-icons/People';
 import Button from 'material-ui/Button';
 import TextField from 'material-ui/TextField';
 import Paper from 'material-ui/Paper';
+
+
+const MAX_ADULTS = 30;
 
 @inject('experience')
 @inject('booking')
@@ -39,7 +41,8 @@ class BookingsNew extends Component {
         complement: '',
         dates: moment()
       },
-      activeStep: 0
+      activeStep: 0,
+      disabled: false
     }
   }
 
@@ -50,11 +53,12 @@ class BookingsNew extends Component {
 
 
   submit() {
+    this.setState({disabled: true});
     const body = Object.assign({}, this.state.values); //Using assign 'cause I don't wanna change the original object
     body.experience_id = this.props.experience.selected.id;
     body.dates = body.dates.format('YYYY-MM-DD');
     this.props.booking.create({}, body, {
-      201: booking =>{
+      201: booking => {
         browserHistory.push(`/bookings/${booking.id}`)
       }
     })
@@ -67,7 +71,8 @@ class BookingsNew extends Component {
     });
   };
 
-  handleNext = () => {
+  handleNext = (e) => {
+    e.preventDefault();
     const { activeStep } = this.state;
     let { name, email, phone } = this.state.values;
     const steps = getSteps();
@@ -90,6 +95,13 @@ class BookingsNew extends Component {
     if (e._isAMomentObject) {
       const values = Object.assign(this.state.values, { dates: e }) //values RECEIVES THE STATE WITH THE NEW MODIFIED ATTRIBUTES
       this.setState({ values });
+    } else if (e.target.name === 'adults') { //ADULTS VALIDATIONS
+      let adults = Number(e.target.value);
+      if (adults === 0) adults = 1;
+      if (adults < 0) adults *= -1;
+      if (adults > 30) adults = MAX_ADULTS;
+      const values = Object.assign(this.state.values, { adults }) //values RECEIVES THE STATE WITH THE NEW MODIFIED ATTRIBUTES
+      this.setState({ values });
     } else {
       const values = Object.assign(this.state.values, { [e.target.name]: e.target.value }) //values RECEIVES THE STATE WITH THE NEW MODIFIED ATTRIBUTES
       this.setState({ values });
@@ -106,6 +118,7 @@ class BookingsNew extends Component {
               <TextField
                 name='address'
                 fullWidth
+                required
                 label='Hotel / Endereço'
                 value={this.state.values.address}
                 onChange={(e) => this.change(e)}
@@ -129,7 +142,7 @@ class BookingsNew extends Component {
   }
 
   getFinalPrice() {
-    return this.props.experience.selected.price*this.state.values.adults
+    return this.props.experience.selected.price * this.state.values.adults
   };
 
   getStepContent(step) {
@@ -195,6 +208,7 @@ class BookingsNew extends Component {
                     fullWidth
                     name='email'
                     label='Email'
+                    type='email'
                     value={this.state.values.email}
                     onChange={(e) => this.change(e)}
                   />
@@ -279,57 +293,60 @@ class BookingsNew extends Component {
         <div className='container'>
           <div className='row'>
             <div className='col-md-8 col-sm-12'>
-              {this.getStepContent(activeStep)}
-              <div className="flow-container text-center">
-                <Button
-                  className="m-3"
-                  disabled={activeStep === 0}
-                  onClick={this.handleBack}
-                >
-                  Voltar
-                </Button>
-                <Button
-                  className="m-3"
-                  variant="raised"
-                  color="primary"
-                  onClick={this.handleNext}
-                >
-                  {activeStep === steps.length - 1 ? 'Confirmar' : 'Avançar'}
-                </Button>
-              </div>
+              <form onSubmit={this.handleNext}>
+                {this.getStepContent(activeStep)}
+                <div className="flow-container text-center">
+                  <Button
+                    className="m-3"
+                    disabled={activeStep === 0}
+                    onClick={this.handleBack}
+                  >
+                    Voltar
+                  </Button>
+                  <Button
+                    className="m-3"
+                    variant="raised"
+                    color="primary"
+                    type="submit"
+                    disabled = {this.state.disabled}
+                  >
+                    {activeStep === steps.length - 1 ? 'Confirmar' : 'Avançar'}
+                  </Button>
+                </div>
+              </form>
             </div>
             <div className='col-sm-4'>
               <Paper elevation={6} style={{ borderRadius: "4px" }} className="p-3 mb-5">
                 <ul className="list-unstyled">
                   <li className="title">{this.props.experience.selected.name}</li>
-                  <li><span className="icon"><Location fontSize /> </span><span className="icon-text">{this.props.experience.selected.location}</span></li>
-                  <li><span className="icon"><Date fontSize /> </span><span className="icon-text">{this.state.values.dates.format("LL")}</span></li>
+                  <li><span className="icon"><Location /> </span><span className="icon-text">{this.props.experience.selected.location}</span></li>
+                  <li><span className="icon"><Date /> </span><span className="icon-text">{this.state.values.dates.format("LL")}</span></li>
                 </ul>
                 <div>
                   <div></div>
                   <div>{this.props.experience.selected.cost}</div>
                 </div>
                 <div className="confirm-container">
-                    <div className='row'>
-                      <div className='col-5'>
-                        {this.state.values.adults} Adultos
-                      </div>
-                      <div className='col-2 text-center'>
-                          x
-                      </div>
-                      <div className='col-5 text-right'>
-                        <Currency quantity={this.props.experience.selected.price} currency='BRL' />
-                      </div>
+                  <div className='row'>
+                    <div className='col-5'>
+                      {this.state.values.adults} {this.state.values.adults > 1 ? 'Adultos' : 'Adulto'}
                     </div>
+                    <div className='col-2 text-center'>
+                      x
+                      </div>
+                    <div className='col-5 text-right'>
+                      <Currency quantity={Number(this.props.experience.selected.price)} currency='BRL' />
+                    </div>
+                  </div>
                 </div>
                 <div className="total-container">
-                  <hr/>
+                  <hr />
                   <div className='row'>
                     <div className='col-6'>
                       <span>Total</span>
                     </div>
                     <div className='col-6 text-right'>
-                      <span><Currency quantity={this.getFinalPrice()} currency='BRL' /></span>
+                      <span><Currency quantity={Number(this.getFinalPrice())} currency='BRL' /></span>
                     </div>
                   </div>
                 </div>
